9ddb7e48121dc183b9fd72a418a0ffd4d8deb93e
Anastasia Sannikova <asyadol15@gmail.com>
2024-09-30 14:24:38 +0300

    fix component container

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,12 +1,12 @@
-import { FC, ReactNode } from 'react';
+import { createElement, FC, ReactNode } from 'react';
 import { clsx } from 'clsx';
 import styles from './styles.module.css';
 
 interface ContainerProps {
   children?: ReactNode;
   className?: string;
+  component?: 'div' | 'section';
 }
 
-export const Container: FC<ContainerProps> = ({ children, className }) => (
-  <div className={clsx(styles.container, className)}>{children}</div>
-);
+export const Container: FC<ContainerProps> = ({ children, className, component = 'div' }) =>
+  createElement(component, { className: clsx(styles.container, className) }, children);
--- src/components/steps/index.tsx
+++ src/components/steps/index.tsx
@@ -8,53 +8,51 @@ import styles from './styles.module.css';
 import { Typography } from '../typography';
 
 export const Steps: FC = () => (
-  <section>
-    <Container>
-      <Typography variant="h2" className={styles.title}>
-        Как это работает
-      </Typography>
-      <div className={styles.wrapper}>
-        <article className={styles.card}>
-          <img className={styles.image} src={waiting} alt="Стрелочка и синие часы" />
-          <Typography variant="h6">Прочитай задание внимательно</Typography>
-          <Typography variant="body14" className={styles.text}>
-            Думаю у тебя не займет это больше двух-трех минут
-          </Typography>
-        </article>
-        <article className={styles.card}>
-          <img
-            className={styles.image}
-            src={deliveryTrack}
-            alt="Фургон с кабиной синего цвета и синяя галочка над ним"
-          />
-          <Typography variant="h6">Изучи весь макет заранее</Typography>
-          <Typography variant="body14" className={styles.text}>
-            Подумай как это будет работать на разных разрешениях и при скролле
-          </Typography>
-        </article>
-        <article className={styles.card}>
-          <img
-            className={styles.image}
-            src={secure}
-            alt="Телефон со стрелочным графиком, а над ним маленький синий щит"
-          />
-          <Typography variant="h6">Сделай хорошо</Typography>
-          <Typography variant="body14" className={styles.text}>
-            Чтобы мы могли тебе доверить подобные задачи в будущем
-          </Typography>
-        </article>
-        <article className={styles.card}>
-          <img
-            className={styles.image}
-            src={moneyBags}
-            alt="Три мешочка, два серых на заднем фоне, один синий на переднем"
-          />
-          <Typography variant="h6">Получи предложение</Typography>
-          <Typography variant="body14" className={styles.text}>
-            Ну тут все просто, не я придумал правила, но думаю так и будет)))
-          </Typography>
-        </article>
-      </div>
-    </Container>
-  </section>
+  <Container component="section">
+    <Typography variant="h2" className={styles.title}>
+      Как это работает
+    </Typography>
+    <div className={styles.wrapper}>
+      <article className={styles.card}>
+        <img className={styles.image} src={waiting} alt="Стрелочка и синие часы" />
+        <Typography variant="h6">Прочитай задание внимательно</Typography>
+        <Typography variant="body14" className={styles.text}>
+          Думаю у тебя не займет это больше двух-трех минут
+        </Typography>
+      </article>
+      <article className={styles.card}>
+        <img
+          className={styles.image}
+          src={deliveryTrack}
+          alt="Фургон с кабиной синего цвета и синяя галочка над ним"
+        />
+        <Typography variant="h6">Изучи весь макет заранее</Typography>
+        <Typography variant="body14" className={styles.text}>
+          Подумай как это будет работать на разных разрешениях и при скролле
+        </Typography>
+      </article>
+      <article className={styles.card}>
+        <img
+          className={styles.image}
+          src={secure}
+          alt="Телефон со стрелочным графиком, а над ним маленький синий щит"
+        />
+        <Typography variant="h6">Сделай хорошо</Typography>
+        <Typography variant="body14" className={styles.text}>
+          Чтобы мы могли тебе доверить подобные задачи в будущем
+        </Typography>
+      </article>
+      <article className={styles.card}>
+        <img
+          className={styles.image}
+          src={moneyBags}
+          alt="Три мешочка, два серых на заднем фоне, один синий на переднем"
+        />
+        <Typography variant="h6">Получи предложение</Typography>
+        <Typography variant="body14" className={styles.text}>
+          Ну тут все просто, не я придумал правила, но думаю так и будет)))
+        </Typography>
+      </article>
+    </div>
+  </Container>
 );
