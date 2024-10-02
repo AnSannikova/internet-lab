84bd8d574c0c218e94b81d922ea610f0f7fbc1ae
Anastasia Sannikova <asyadol15@gmail.com>
2024-10-02 10:28:14 +0300

    fix component logo

--- src/components/ui/logo/index.tsx
+++ src/components/ui/logo/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import logo from '../../assets/logo.svg';
+import logo from '@/assets/logo.svg';
 import styles from './styles.module.css';
 
 export const Logo: FC = () => (
